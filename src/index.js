3eadaed6d43cae655359bbb988608f24a52943e3
Alex Cui <cuizhihui030925@outlook.com>
2020-11-28 10:35:20 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,72 @@
+const ClipCC = require('clipcc-extension');
+
+class SampleExtension extends ClipCC.Extension {
+    get(json, key) {
+        let parsedObject = JSON.parse(json);
+        const parsedParam = key.split('.');
+        for (const param of parsedParam) {
+            parsedObject = parsedObject[param];
+        }
+        return JSON.stringify(parsedObject);
+    }
+
+    set(json, key, value) {
+        const parsedObject = JSON.parse(json);
+        const parsedParam = key.split('.');
+        let tempObject = parsedObject;
+        for (let i = 0; i < parsedParam.length - 1; i++) {
+            tempObject = tempObject[parsedParam[i]];
+        }
+        tempObject[parsedParam[parsedParam.length - 1]] = value;
+        return JSON.stringify(parsedObject);
+    }
+
+    init() {
+        ClipCC.API.addCategory({
+            categoryId: 'alexcui.json.json',
+            messageId: 'JSON',
+            color: '#FFB11B'
+        });
+        ClipCC.API.addBlock({
+            opcode: 'alexcui.json.get',
+            type: ClipCC.Type.BlockType.REPORTER,
+            messageId: 'alexcui.json.get', // get key [KEY] from [JSON]
+            categoryId: 'alexcui.json.json',
+            function: args => this.get(args.JSON, args.KEY),
+            argument: {
+                KEY: {
+                    type: ClipCC.Type.ArgumentType.STRING,
+                    default: ''
+                },
+                JSON: {
+                    type: ClipCC.Type.ArgumentType.STRING,
+                    default: ''
+                }
+            }
+        });
+
+        ClipCC.API.addBlock({
+            opcode: 'alexcui.json.set',
+            type: ClipCC.Type.BlockType.REPORTER,
+            messageId: 'alexcui.json.set', // set key [KEY] to [VALUE] in [JSON]
+            categoryId: 'alexcui.json.json',
+            function: args => this.set(args.JSON, args.KEY, args.VALUE),
+            argument: {
+                KEY: {
+                    type: ClipCC.Type.ArgumentType.STRING,
+                    default: 'value'
+                },
+                VALUE: {
+                    type: ClipCC.Type.ArgumentType.STRING,
+                    default: ' '
+                },
+                JSON: {
+                    type: ClipCC.Type.ArgumentType.STRING,
+                    default: ''
+                }
+            }
+        })
+    }
+}
+
+module.exports = SampleExtension;
